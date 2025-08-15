0882a98f01957fdd1bf90615718b780c1c49f922
Parineeta Hulle <parineetahulle@gmail.com>
2025-08-15 10:46:07 +0530

    Add files via upload

---
+++ test3/looping.py
@@ -0,0 +1,9 @@
+#print prime number
+i= 1
+num = 1
+while( num<=10):
+    print(num * 2)
+    i *=1
+
+
+
---
+++ test3/sum of series.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the series:"))
+i = 1
+fact=1
+while(fact <= i):
+    print(fact % i ==0)
+    i+=1
